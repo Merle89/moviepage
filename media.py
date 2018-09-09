5b0c6e2155b79d0ef5fd6bce2b38d30c6b4db608
Merle89 <40489780+Merle89@users.noreply.github.com>
2018-09-09 13:24:26 +0200

    Update media.py

--- media.py
+++ media.py
@@ -1,13 +1,15 @@
+#Defines the Movie class that contains the details of a movie.
 import webbrowser
 
 # Class for Movies including Title, Storyline, Poster Image URL and Trailer
 class Movie():
        def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+# Initialises Movie class instance variables.
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-# Show the trailers by clicking on the posterimage
+# Shows the trailers by clicking on the posterimage
 def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
